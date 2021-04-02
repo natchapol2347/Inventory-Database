f942e4806553ab206ee1ea3615509698b4da73f1
ohmanurak <54495874+ohmanurak@users.noreply.github.com>
2021-04-02 20:25:22 +0700

    Update oneServer.go
    edit the response of the server

--- oneServer.go
+++ oneServer.go
@@ -34,15 +34,30 @@ func handleClientRequest(con net.Conn) {
 	for {
 		// Waiting for the client request
 		clientRequest, err := clientReader.ReadString('\n')
- 
+		message:="Please provide numbers 1-5"
 		switch err {
 		case nil:
 			clientRequest := strings.TrimSpace(clientRequest)
-			if clientRequest == ":QUIT" {
+			if clientRequest == "QUIT" {
 				log.Println("client requested server to close the connection so closing")
 				return
-			} else {
-				log.Println(clientRequest)
+			}else if clientRequest == "1"{
+				log.Println("Insert items")
+				message="Insert items"
+			}else if clientRequest == "2"{
+				log.Println("Remove items")
+				message="Remove items"
+			}else if clientRequest == "3"{
+				log.Println("Check current stock")
+				message="Check current stock"
+			}else if clientRequest == "4"{
+				log.Println("Check record for insert")
+				message="Check record for insert"
+			}else if clientRequest == "5"{
+				log.Println("Check record for remove")
+				message="Check record for remove"
+			}else {
+				log.Println("Please provide numbers 1-5")
 			}
 		case io.EOF:
 			log.Println("client closed the connection by terminating the process")
@@ -53,7 +68,8 @@ func handleClientRequest(con net.Conn) {
 		}
  
 		// Responding to the client request
-		if _, err = con.Write([]byte(clientRequest)); err != nil {
+		_, err = con.Write([]byte(message+"\n"))
+		if err != nil {
 			log.Printf("failed to respond to client: %v\n", err)
 		}
 	}
