355e8a8f476bec456479e58066d3b3b95381b37f
Natchapol Watthanawong <pupreez@gmail.com>
2021-04-04 19:50:23 +0700

    joining cache and database codes, added name attribute to cacheItem and cache functions, attempting to use goroutine to concurrently run, one process returns to request and one process modifies cache (write through)

---
+++ cacheandDB.go
@@ -0,0 +1,347 @@
+package main
+
+import (
+
+	"errors"
+	"fmt"
+	// "mime/multipart"
+	"sync"
+	"database/sql"
+	"strconv"
+	"time"
+	"log"
+
+	_"github.com/go-sql-driver/mysql"
+	// "github.com/golang/glog"
+
+)
+
+//Cache codes
+type Cache struct {
+	capacity int
+	size int
+	items    map[int]*cacheItem
+	mu       sync.Mutex
+	head     *cacheItem
+	tail     *cacheItem
+}
+
+type cacheItem struct {
+	name     string
+	serial   int
+	quantity int
+	next     *cacheItem
+	prev	 *cacheItem
+}
+
+func newCache(c int) *Cache {
+	return &Cache{
+		capacity: c,
+		size: 0,
+		items:    make(map[int]*cacheItem),
+		mu:       sync.Mutex{},
+		head:     nil,
+		tail: 	  nil,
+	}
+}
+
+func newItemNode(in_name string, key int, value int) *cacheItem{
+	return &cacheItem{
+		name: in_name,
+		serial: key,
+		quantity: value,
+		next: nil,
+		prev: nil,
+	}
+}
+func (c *Cache) insert_tail(name string,key int, value int) *cacheItem{
+	//make new item from argument
+	newItem := newItemNode(name,key, value)
+	if(c.tail == nil && c.head == nil){
+		c.tail = newItem
+		c.head = newItem
+
+	}else{
+		newItem.next = c.tail
+		c.tail.prev = newItem
+		c.tail = newItem
+	}
+
+	return newItem
+
+}
+
+func (c *Cache) moveToFront(node *cacheItem){
+	if node == c.tail{
+		return 
+	}else if node == c.head{
+		c.head = c.head.prev
+		//last node's next must point to nil
+		c.head.next = nil 
+	}else{
+		node.prev.next = node.next
+		node.next.prev = node.prev
+	}
+
+	node.next = c.tail
+	c.tail.prev = node
+	c.tail = node
+}
+
+func (c *Cache) pop(){
+	if c.head == nil && c.tail == nil{
+		return
+	}else if c.head == c.tail{
+		c.head, c.tail = nil, nil
+	}else{
+		c.head = c.head.prev
+		c.head.next = nil
+	}
+}
+func (c *Cache) printCache(){
+	current := c.tail
+	var i int;
+	for i=0;i<=c.size;i++{
+		if(current != nil){		
+			fmt.Printf("id:%d|,quantity:%d|size:%d| ->", current.serial, current.quantity,c.size)
+			current = current.next
+		}
+	}
+	fmt.Println("\n")
+}
+
+
+func (c *Cache) get(end chan int, name string, key int, load int) (string, int,int){
+	if _, ok := c.items[key]; ok{
+		value := c.items[key].quantity
+		if(value - load < 0){
+			// fmt.Println("not enough in stock")
+			return "",-1,-1
+		}
+		c.moveToFront(c.items[key])
+		c.items[key].quantity -= load
+		end := make(chan int) 
+		go going_out(end, name, key, load)
+		return name, key, value-load
+	}else{
+		//if there's no key
+		// fmt.Println("there's no key", key, "yet")
+		return "",-1,-1
+	}
+}
+
+func (c *Cache) put(name string,key int, load int) {
+	//if there's already key in cache just add 
+	if _, ok := c.items[key]; ok {
+		c.items[key].quantity += load
+		c.moveToFront(c.items[key])
+		end := make(chan int) 
+		go going_in(end, name, key, load)
+		return
+	}
+
+	if c.size == c.capacity {
+		delKey := c.head.serial
+		c.pop()
+		c.size--
+		delete(c.items, delKey)
+	}
+	page := c.insert_tail(name, key, load)
+	c.size++
+	c.items[key] = page
+	
+}
+
+
+
+//Database code
+var (
+	db    *sql.DB
+	mutex sync.Mutex
+)
+
+func get_items(q chan int, e chan int, n chan string, id int) {
+
+	row, err := db.Query("SELECT name, quantity, expdate FROM items WHERE id = " + strconv.Itoa(id))
+	if err != nil {
+		panic(err)
+	}
+	defer row.Close()
+	for row.Next() {
+		var (
+			name     string
+			quantity int
+			expdate  int
+		)
+		row.Scan(&name, &quantity, &expdate)
+		q <- quantity
+		n <- name
+		e <- expdate
+		fmt.Println("name: ", name, " quantity: ", quantity, " expdate: ", expdate)
+	}
+}
+func decrement(q chan int, c chan int, quantity int, id int) {
+	quantityy := <-q // channel from get_items
+	newQuantity := quantityy - quantity
+	if newQuantity < 0 {
+		c <- 0
+		return
+	}
+	fmt.Println("the items left in stock: ", newQuantity)
+	db.Exec("update items set quantity = ? where id = ? ", newQuantity, id)
+	c <- 0
+}
+
+func insertingex(n chan string, e chan int, quantity int, id int, name string) {
+	product := <-n
+	expdate := <-e
+	db.Exec("INSERT INTO export(name, quantity, expdate,id,user) VALUES (?, ?, ?, ?, ?)", product, quantity, expdate, id, name)
+}
+
+func going_out(end chan int, name string, id int, quantity int) {
+	// fmt.Printf("start\n")
+	start := time.Now()
+	c := make(chan int)
+	q := make(chan int)
+	e := make(chan int)
+	n := make(chan string)
+	if rowExists("SELECT * FROM items WHERE id = ?", id) {
+		mutex.Lock()
+		go get_items(q, e, n, id)
+		go decrement(q, c, quantity, id)
+		<-c // wait for all go routines
+		mutex.Unlock()
+	} else {
+		return
+	}
+	go insertingex(n, e, quantity, id, name)
+	fmt.Printf("time: %v\n", time.Since(start))
+
+	num, _ := strconv.Atoi(name)
+	end <- num
+	return
+}
+
+func increment(q chan int, c chan int, quantity int, id int) {
+	quantityy := <-q // channel from get_items
+	newQuantity := quantityy + quantity
+	if newQuantity < 0 {
+		c <- 0
+		return
+	}
+	fmt.Println("the items left in stock: ", newQuantity)
+	db.Exec("update items set quantity = ? where id = ? ", newQuantity, id)
+	c <- 0
+}
+func insertingim(n chan string, e chan int, quantity int, id int, name string) {
+	product := <-n
+	expdate := <-e
+	db.Exec("INSERT INTO import(name, quantity, expdate,id,user) VALUES (?, ?, ?, ?, ?)", product, quantity, expdate, id, name)
+}
+
+func going_in(end chan int, name string, id int, quantity int) {
+	start := time.Now()
+	c := make(chan int)
+	q := make(chan int)
+	e := make(chan int)
+	n := make(chan string)
+	if rowExists("SELECT * FROM items WHERE id = ?", id) {
+		mutex.Lock()
+		go get_items(q, e, n, id)
+		go increment(q, c, quantity, id)
+		<-c // wait for all go routines
+		mutex.Unlock()
+	} else {
+		insertingitem("New  with id "+strconv.Itoa(id), quantity, 0, id)
+	}
+
+	go insertingim(n, e, quantity, id, name)
+	fmt.Printf("time: %v\n", time.Since(start))
+
+	num, _ := strconv.Atoi(name)
+	end <- num
+	return
+}
+
+func rowExists(query string, args ...interface{}) bool {
+	var exists bool
+	query = fmt.Sprintf("SELECT exists (%s)", query)
+	err := db.QueryRow(query, args...).Scan(&exists)
+	if err != nil && err != sql.ErrNoRows {
+		log.Fatal("error checking if row exists '%s' %v", args, err)
+	}
+	return exists
+}
+
+func insertingitem(name string, quantity int, expdate int, id int) {
+	db.Exec("INSERT INTO items(name,quantity,expdate,id) VALUES (?,?,?,?)", name, quantity, expdate, id)
+}
+
+func show_current(endrec chan int, name string) {
+	rows, err := db.Query("SELECT * FROM items")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("current items: \n")
+	for rows.Next() {
+		var name string
+		var quantity int
+		var expdate int
+		var id int
+		err = rows.Scan(&name, &quantity, &expdate, &id)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("name: ", name, "\t quantity: ", quantity, "\t expdate: ", expdate, "\t id: ", id,"\n")
+	}
+	num, _ := strconv.Atoi(name)
+	endrec<-num
+}
+
+func show_record_in(endrec chan int, name string) {
+	rows, err := db.Query("SELECT * FROM import")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("record in: \n")
+	for rows.Next() {
+		var name string
+		var quantity int
+		var expdate int
+		var id int
+		var user string
+		err = rows.Scan(&name, &quantity, &expdate, &id, &user)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("name: ", name, "\t quantity: ", quantity, "\t expdate: ", expdate, "\t id: ", id,"\t user: ", user,"\n")
+	}
+	num, _ := strconv.Atoi(name)
+	endrec<-num
+}
+
+func show_record_out(endrec chan int, name string) {
+	rows, err := db.Query("SELECT * FROM export")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("record out: \n")
+	for rows.Next() {
+		var name string
+		var quantity int
+		var expdate int
+		var id int
+		var user string
+		err = rows.Scan(&name, &quantity, &expdate, &id, &user)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("name: ", name, "\t quantity: ", quantity, "\t expdate: ", expdate, "\t id: ", id,"\t user: ", user,"\n")
+	}
+	num, _ := strconv.Atoi(name)
+	endrec<-num
+}
+
+func main{
+
+}
