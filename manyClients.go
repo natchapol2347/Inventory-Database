52a6fe50d0e6cfeef9cb79df321fb9053c406224
ohmanurak <54495874+ohmanurak@users.noreply.github.com>
2021-04-06 10:50:07 +0700

    generate concurrent client with go routine

--- manyClients.go
+++ manyClients.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"bufio"
-	// "fmt"
-	"math/rand"
-	"strconv"
+	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net"
-	// "time"
+	"strconv"
+	"time"
+
 	// "os"
 	"strings"
 )
 
-
-//Old file of client
 func main() {
 	con, err := net.Dial("tcp", "0.0.0.0:9999")
 	if err != nil {
@@ -22,29 +21,42 @@ func main() {
 	}
 	defer con.Close()
 
-	serverReader := bufio.NewReader(con)
+	n := 2000
+	start_whole := time.Now()
+	end := make(chan int, n)
+	for i := 0; i < n; i++ {
 
-	for i:=0; i<100; i++{
 		// Waiting for the client request
-		clientRequest:= strconv.Itoa(rand.Intn(4)+1) //Randomize 5 numbers
-			if _, err = con.Write([]byte(clientRequest + "\n")); err != nil {
-				log.Printf("failed to send the client request: %v\n", err)
-			}
-		
+		go client(end, con, err,start_whole)
 
-		
-		// Waiting for the server response
-		serverResponse, err := serverReader.ReadString('\n')
- 
-		switch err {
-		case nil:
-			log.Println(strings.TrimSpace(serverResponse))
-		case io.EOF:
-			log.Println("server closed the connection")
-			return
-		default:
-			log.Printf("server error: %v\n", err)
-			return
-		}
 	}
+	for i := 0; i < n; i++ {
+		<-end
+	}
+	fmt.Printf("Total time: %v\n", time.Since(start_whole))
+}
+
+func client(end chan int, con net.Conn, err error, start time.Time) {
+	start_each := time.Now()
+	serverReader := bufio.NewReader(con)
+	clientRequest := strconv.Itoa(rand.Intn(2) + 1)
+	if _, err = con.Write([]byte(clientRequest + "\n")); err != nil {
+		log.Printf("failed to send the client request: %v\n", err)
+	}
+
+	// Waiting for the server response
+	serverResponse, err := serverReader.ReadString('\n')
+
+	switch err {
+	case nil:
+		log.Println(strings.TrimSpace(serverResponse))
+	case io.EOF:
+		log.Println("server closed the connection")
+		return
+	default:
+		log.Printf("server error: %v\n", err)
+		return
+	}
+	end <- 0
+	fmt.Printf("time each: %v\n", time.Since(start)-time.Since(start_each))
 }
