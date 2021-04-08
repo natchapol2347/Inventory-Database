b552232337b949cb90f35abba105cf6c660c6f3b
Natachai Boonprasert 62011157 <72903545+dunknatachai@users.noreply.github.com>
2021-04-09 00:42:18 +0700

    Update concurrent_test_drive.go

--- concurrent_test_drive.go
+++ concurrent_test_drive.go
@@ -2,44 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	fmt.Println("====== Start main ======")
-	name := "Fake name"
 
-	result := make(chan string)
-	go hello(name, result)
-
-	fmt.Println("Finish main")
-
-	fmt.Println(<-result)
-
+	// declare channel
 	my_res := make(chan string)
-	go checkin("123", 2, my_res)
+	// test concurrent
+	for i := 0; i < 10; i++ {
+		// call function
+		go checkin("123", 2, my_res)
+	} //. End for
 
+	// display my_res on screen
 	fmt.Println(<-my_res)
-}
 
-func hello(name string, result chan<- string) {
-	output := "Hello " + name
-	fmt.Printf("In function = %s\n", output)
-	result <- output
-}
-
-func checkin(itmNo string, q int16, result chan<- string) {
-	start2 := time.Now() //start what time
-	fmt.Print(start2)    //display time
-	itemCode := itmNo
-	qty := q
-
-	// print display on screen
-	fmt.Printf("Item added %s qty=%d\n", itemCode, qty)
-	start2 = time.Now() //start what time
-	fmt.Print(start2)   //display time
-
-	my_res := "OK : MyRes"
-
-	result <- my_res
-} //. End checkin
+	// display text on screen
+	fmt.Println("====== Start main ======")
+} // .End function
