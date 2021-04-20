ba8262e6fd9a363d37ecf7575fc484a0e8249f40
Natchapol Watthanawong <pupreez@gmail.com>
2021-04-21 02:23:27 +0700

    trying to run goroutines with cache

--- concurrentLRU.go
+++ concurrentLRU.go
@@ -3,12 +3,17 @@ package main
 import (
 
 	// "errors"
+	// "context"
 	"fmt"
+	"log"
 	"time"
+
 	// "log"
 	// "mime/multipart"
 	"sync"
 	// "time"
+	"math/rand"
+	"strconv"
 )
 
 type Cache struct {
@@ -18,6 +23,7 @@ type Cache struct {
 	head     *cacheItem
 	tail     *cacheItem
 	mu 		sync.Mutex
+
 }
 
 type cacheItem struct {
@@ -111,29 +117,35 @@ func (c *Cache) printCache(){
 }
 
 
-func (c *Cache) get(name string, key int, load int) (int,int){
+func (c *Cache) get(endKey chan int, endQty chan int,name string, key int, load int){
 	c.mu.Lock()
 	if res, ok := c.items[key]; ok{
 		c.mu.Unlock()	
 		value := res.quantity
 		if(value - load < 0){
 			// fmt.Println("not enough in stock")
-			return -1,-1
+			
 		}
 		c.promote(res)
 		c.mu.Lock()
 		c.items[key].quantity -= load 
 		c.mu.Unlock()
-		return key, value-load
+		
+			endKey <- key
+			endQty <- value-load
+		
+		
+		 
 	}else{
 		c.mu.Unlock()
 		//if there's no key
 		// fmt.Println("there's no key", key, "yet")
-		return -1,-1
+		endKey <- -1
+		endQty <- -1
 	}
 }
 
-func (c *Cache) put(name string, key int, load int) {
+func (c *Cache) put(endSig chan int, name string, key int, load int) {
 	c.mu.Lock()
 	if _, ok := c.items[key]; ok {
 		c.mu.Unlock()
@@ -143,7 +155,7 @@ func (c *Cache) put(name string, key int, load int) {
 		c.mu.Unlock()
 		c.promote(c.items[key])
 		
-		return
+		
 	}
 	c.mu.Unlock()
 
@@ -176,31 +188,44 @@ func (c *Cache) promote(node *cacheItem) {
 	}
 	
   }
+// func (c *Cache) free(){
+
+// }
+
 
 func main() {
-	cache := newCache(3)
-	// cache.put(2, 2)
-	// cache.get(2,100)
-	// cache.get(1,100)
-	// cache.put(1, 1)
-	// cache.put(1, 5)
-	// cache.get(1, 5)
-	// cache.get(2,25)
-	// cache.put(8, 8)
-	// cache.get(1, 3)
-	// cache.get(8, 5)
-	// cache.put(2,500)
-	// cache.get(2,400)
+	
+	start := time.Now()
+	n := 200
+	cache := newCache(n)
+	c := make(chan int, n)
+	for i:=0; i<n; i++ {
+		id := rand.Intn(100)
+		name := strconv.Itoa(id)
+		go cache.put(c, name,id, 100)
+
+		select{
+		case sig := <- c:
+			log.Println(sig)
+		default:
+			log.Println("k")
+		}
+	
+
+		
+	}
+	
+	// cache.put("toy",1,4)
 	// cache.printCache()
-	cache.put("toy",1,4)
-	cache.printCache()
-	cache.put("diamond",2,45)
-	cache.printCache()
-	cache.put("gucci",23,3247)
-	cache.printCache()
-	cache.get("diamond",2,30)
-	cache.printCache()
-	cache.put("car",50,223)
-	cache.printCache()
+	// cache.put("diamond",2,45)
+	// cache.printCache()
+	// cache.put("gucci",23,3247)
+	// cache.printCache()
+	// cache.get("diamond",2,30)
+	// cache.printCache()
+	// cache.put("car",50,223)
+	elapsed := time.Since(start)
+	log.Println(elapsed)
+	// cache.printCache()
 
 }
