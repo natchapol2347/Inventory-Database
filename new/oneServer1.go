d446ef081f57cdaf876e23f4a48e711d2f0a3276
Natchapol Watthanawong <pupreez@gmail.com>
2021-04-25 23:56:41 +0700

    s

--- new/oneServer1.go
+++ new/oneServer1.go
@@ -334,3 +334,223 @@ func Show_record_out(endrec chan int, name string,me chan string) {
 	num, _ := strconv.Atoi(name)
 	endrec <- num
 }
+
+// //Cache codes
+// type Cache struct {
+// 	capacity int
+// 	size int
+// 	items    map[int]*cacheItem
+// 	mu       sync.Mutex
+// 	head     *cacheItem
+// 	tail     *cacheItem
+// }
+
+// type cacheItem struct {
+// 	name     string
+// 	serial   int
+// 	quantity int
+// 	next     *cacheItem
+// 	prev	 *cacheItem
+// 	mu       sync.Mutex
+// 	last_promoted time.Time
+// }
+
+// func newCache(c int) *Cache {
+// 	return &Cache{
+// 		capacity: c,
+// 		size: 0,
+// 		items:    make(map[int]*cacheItem),
+// 		mu:       sync.Mutex{},
+// 		head:     nil,
+// 		tail: 	  nil,
+// 	}
+// }
+
+// func newItemNode(in_name string, key int, value int) *cacheItem{
+// 	return &cacheItem{
+// 		name: in_name,
+// 		serial: key,
+// 		quantity: value,
+// 		next: nil,
+// 		prev: nil,
+// 		mu :     sync.Mutex{},
+// 		last_promoted: time.Time{},
+// 	}
+// }
+// func (c *Cache) insert_tail(name string,key int, value int) *cacheItem{
+// 	//make new item from argument
+// 	newItem := newItemNode(name, key, value)
+// 	if(c.tail == nil && c.head == nil){
+// 		c.tail = newItem
+// 		c.head = newItem
+
+// 	}else{
+// 		newItem.next = c.tail
+// 		c.tail.prev = newItem
+// 		c.tail = newItem
+// 	}
+
+// 	return newItem
+
+// }
+
+// func (c *Cache) moveToFront(node *cacheItem){
+// 	if node == c.tail{
+// 		return 
+// 	}else if node == c.head{
+// 		c.head = c.head.prev
+// 		//last node's next must point to nil
+// 		c.head.next = nil 
+// 	}else{
+// 		node.prev.next = node.next
+// 		node.next.prev = node.prev
+// 	}
+
+// 	node.next = c.tail
+// 	c.tail.prev = node
+// 	c.tail = node
+// }
+
+// func (c *Cache) pop(){
+// 	if c.head == nil && c.tail == nil{
+// 		return
+// 	}else if c.head == c.tail{
+// 		c.head, c.tail = nil, nil
+// 	}else{
+// 		c.head = c.head.prev
+// 		c.head.next = nil
+// 	}
+// }
+// func (c *Cache) printCache(){
+// 	current := c.tail
+// 	var i int;
+// 	for i=0;i<=c.size;i++{
+// 		if(current != nil){		
+// 			fmt.Printf("|name:%s|id:%d|,quantity:%d|size:%d| ->", current.name, current.serial, current.quantity,c.size)
+// 			current = current.next
+// 		}
+// 	}
+// 	fmt.Println("\n")
+// }
+
+
+// func (c *Cache) get(end chan int, name string, key int, load int) {
+// 	c.mu.Lock()
+// 	if res, ok := c.items[key]; ok{
+// 		c.mu.Unlock()
+// 		value := res.quantity
+// 		if(value - load < 0){
+// 			// fmt.Println("not enough in stock")
+// 			<- end
+// 			return 
+// 		}
+// 		c.promote(res)
+// 		res.mu.Lock()
+// 		c.items[key].quantity -= load 
+// 		res.mu.Unlock()
+// 		// c.mu.Lock()
+// 		// result <- c.items[key]
+// 		// c.mu.Unlock()
+// 		go going_out(nil, end, name, key, load)
+// 		<- end
+		
+// 		return 
+// 	}else{
+// 		//if there's no key
+// 		// fmt.Println("there's no key", key, "yet")
+// 		c.mu.Unlock()
+// 		x := make(chan bool) 
+// 		defer close(x)
+		
+// 		go going_out(x, end, name, key, load)
+		
+// 		// select{
+// 		// case inDB := <-x:
+// 		// 	{
+// 		// 		if(!inDB){
+// 		// 			log.Println("not here")
+// 		// 			<- end
+// 		// 			return 
+// 		// 		}
+// 		// 	}
+// 		// default:
+// 		// 	{
+// 		// 		log.Println("channel not ready")
+// 		// 	}
+			
+
+// 		// }
+// 		if(!<-x){
+// 			log.Println("not here")
+// 			<-end
+// 			return
+
+// 		}else{
+// 			log.Println("channel not ready")
+// 		}
+
+// 		log.Println("kksdf")
+// 		c.put(end, name, key, load)
+// 		// c.mu.Lock()
+// 		// result <- c.items[key]
+// 		// c.mu.Unlock()
+// 		fmt.Println("kksdf")
+// 		<- end
+// 		return 
+		
+// 	}
+	
+// }
+
+// func (c *Cache) put(end chan int, name string,key int, load int) {
+// 	//if there's already key in cache just add 
+// 	c.mu.Lock()
+// 	if res, ok := c.items[key]; ok {
+// 		c.mu.Unlock()
+// 		res.mu.Lock()
+// 		res.quantity += load
+// 		res.mu.Unlock()
+// 		c.promote(c.items[key])
+// 		fmt.Println("yoyo")
+// 		go going_in(end, name, key, load)
+// 		<- end
+// 		return
+// 	}
+// 	c.mu.Unlock()
+// 	//if cache is full delete last recent node
+// 	if c.size == c.capacity {
+// 		delKey := c.head.serial
+// 		c.pop()
+// 		c.size--
+// 		c.mu.Lock()
+// 		delete(c.items, delKey)
+// 		c.mu.Unlock()
+// 	}
+// 	//insert new node
+// 	page := c.insert_tail(name, key, load)
+// 	c.size++
+// 	c.mu.Lock()
+// 	c.items[key] = page
+// 	c.mu.Unlock()
+// 	go going_in(end, name, key, load)
+// 	<- end
+// 	return
+
+	
+
+// }
+
+// func (c *Cache) promote(node *cacheItem) {
+// 	now := time.Now()
+// 	stale := now.Add(time.Minute * -1) // if more than one minute has pass allow for promotion
+  
+// 	node.mu.Lock()
+// 	defer node.mu.Unlock()
+// 	if node.last_promoted.Before(stale) {
+// 	  node.last_promoted = now
+// 	  c.mu.Lock()
+// 	  defer c.mu.Unlock()
+// 	  c.moveToFront(node)
+// 	}
+	
+//   } 
